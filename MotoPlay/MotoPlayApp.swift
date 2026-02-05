9c632c2f895feb65e1a3f9a1e174f696a8558188
Kevin Muñoz <tgdkevinmunoz@gmail.com>
2026-02-05 08:35:23 +0000

    Initial Commit

---
+++ MotoPlay/MotoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoPlayApp.swift
+//  MotoPlay
+//
+//  Created by Kevin Muñoz G on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MotoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
